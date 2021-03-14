b4d34bc1564006faca94752e573058fb2b990067
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-03-13 22:26:08 -0500

    add confirms for password criterias

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var passwordMaxLength = 128
 var passwordMinLength = 8
 
+
 //Generate Password code
 
 function getRandomLower() {
@@ -26,15 +27,28 @@ console.log(getRandomSymbol());
 
 // Write password to the #password input
 function promptMe() {
-    const passwordLength = Number(window.prompt("Please enter passsword length. Must be equal to or greater than " + passwordMinLength + " but less than " + passwordMaxLength + " characters.",""));
-    if (passwordLength < 8 || passwordLength > 128) {
+    const passwordLength = Number(window.prompt("Please enter password length. Must be equal to or greater than " + passwordMinLength + " but less than " + passwordMaxLength + " characters.",""));
+    if (passwordLength < passwordMinLength || passwordLength > passwordMaxLength) {
         window.alert("Incorrect value entered!");
       } else {
-  
+        var getRandomLower = window.confirm("Would you like lowercases in your password?")
+        if (getRandomLower = true) {
+            var getRandomUpper = window.confirm("Would you like uppercases in your password?");
+            if(getRandomUpper = true) {
+                var getRandomNumber = window.confirm("Would you like numbers in your password?");
+                if(getRandomNumber = true) {
+                    var getRandomSymbol = window.confirm("Would you like symbols in your password?");
+                }
+                else {
+
+                }
+            } else {
+
+            }
+        } 
+        else {
+        }
       }
-
-
-    console.log(passwordLength)
 }
 
 //function writePassword() {
