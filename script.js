d56d0111041662459e6363ce532ad0522296b2ae
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-03-13 23:41:20 -0500

    add generate password js

--- script.js
+++ script.js
@@ -2,62 +2,58 @@
 var generateBtn = document.querySelector("#generate");
 var passwordMaxLength = 128
 var passwordMinLength = 8
-
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var number = "0123456789"
+var symbol = '!@#$%^&*()-_=+\|]}[{",.></?;:~`\''
 
 //Generate Password code
 
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-    const symbols = '!@#$%^&*()-_=+\|]}[{",.></?;:~`';
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-console.log(getRandomSymbol());
 
 // Write password to the #password input
-function promptMe() {
-    const passwordLength = Number(window.prompt("Please enter password length. Must be equal to or greater than " + passwordMinLength + " but less than " + passwordMaxLength + " characters.",""));
-    if (passwordLength < passwordMinLength || passwordLength > passwordMaxLength) {
+function generatePassword() {
+    var passwordLength = window.prompt("Please enter password length. Must be equal to or greater than " + passwordMinLength + " but less than or equal to " + passwordMaxLength + " characters.","");
+    if ((passwordLength < passwordMinLength || passwordLength > passwordMaxLength) || isNaN(passwordLength)) {
         window.alert("Incorrect value entered!");
       } else {
-        var getRandomLower = window.confirm("Would you like lowercases in your password?")
-        if (getRandomLower = true) {
-            var getRandomUpper = window.confirm("Would you like uppercases in your password?");
-            if(getRandomUpper = true) {
-                var getRandomNumber = window.confirm("Would you like numbers in your password?");
-                if(getRandomNumber = true) {
-                    var getRandomSymbol = window.confirm("Would you like symbols in your password?");
-                }
-                else {
-
-                }
-            } else {
-
-            }
-        } 
-        else {
+        var getRandomLower = window.confirm("Would you like lowercases in your password?");
+        var getRandomUpper = window.confirm("Would you like uppercases in your password?");
+        var getRandomNumber = window.confirm("Would you like numbers in your password?");
+        var getRandomSymbol = window.confirm("Would you like symbols in your password?");
+        console.log(getRandomLower);
+        console.log(getRandomUpper);
+        console.log(getRandomNumber);
+        console.log(getRandomSymbol);
+        
+        var charset = "";
+        if (getRandomLower) {
+            charset += lowercase;
         }
-      }
+        if (getRandomUpper) {
+            charset += uppercase;
+        }
+        if (getRandomNumber) {
+            charset += number;
+        }
+        if (getRandomSymbol) {
+            charset += symbol;
+        }
+        var password = "";
+        for(var i=0; i<passwordLength; ++i) {
+            password += charset.charAt(Math.floor(Math.random()*charset.length));
+        }
+        console.log(password);
+        return password;
+    }
 }
 
-//function writePassword() {
-//  var password = generatePassword();
-//  var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//  passwordText.value = password;
+  passwordText.value = password;
 
-//}
+}
 
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+//Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
